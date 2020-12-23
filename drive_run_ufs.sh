188115b156a15c318e4eeaa3abf8c7584822a304
jian.kuang <Jian.Kuang@noaa.gov>
2020-12-23 10:18:08 -0600

    add INPUT directory

--- drive_run_ufs.sh
+++ drive_run_ufs.sh
@@ -6,11 +6,11 @@ set -eux
 
 MACHINE_ID=orion.intel
 
-#TEST_NAME=fv3_ccpp_control
+TEST_NAME=fv3_ccpp_control
 #TEST_NAME=cpld_control
-TEST_NAME=datm_control_gefs
+#TEST_NAME=datm_control_gefs
 
-UFSsrc=/work/noaa/da/rmahajan/work/UFS/standalone/ufs-weather-model
+UFSsrc=/work/noaa/marine/Jian.Kuang/ufs-weather-model
 
 [[ "$TEST_NAME" = "fv3_ccpp_control" ]] && UFSexe=$UFSsrc/build-fv3/ufs_model
 [[ "$TEST_NAME" = "cpld_control" ]] && UFSexe=$UFSsrc/build-cpld/ufs_model
@@ -23,10 +23,13 @@ UFSsrc=/work/noaa/da/rmahajan/work/UFS/standalone/ufs-weather-model
 
 APRUN="srun --label -n $NTASKS"
 
-RUNDIR=/work/noaa/da/rmahajan/work/UFS/standalone/stmp/$TEST_NAME
+RUNDIR=/work/noaa/marine/Jian.Kuang/dataroot/$TEST_NAME
 
+# These directories are Orion-specific
 RT_COMPILER=$(echo $MACHINE_ID | cut -d. -f2)
 RTPWD=/work/noaa/nems/emc.nemspara/RT/NEMSfv3gfs/develop-20201112/${RT_COMPILER^^}
+INPUTDATA_ROOT=/work/noaa/nems/emc.nemspara/RT/NEMSfv3gfs/input-data-20201220/
+# !These directories are Orion-specific
 
 set +x
 module use $UFSsrc/modulefiles/$MACHINE_ID
@@ -42,5 +45,6 @@ export APRUN
 export RUNDIR
 export RT_COMPILER # needed in stupid default_vars.sh
 export RTPWD
+export INPUTDATA_ROOT
 
 ./run_ufs.sh
